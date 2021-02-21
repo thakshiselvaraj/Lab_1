bcf9056d2a5c5df93baf624f9bd115ff368e515c
thakshiselvaraj <thakshiselvaraj29@gmail.com>
2021-02-21 11:58:23 +0530

    First commit

---
+++ Lab_1/src/Print/p_1.java
@@ -0,0 +1,12 @@
+package Print;
+
+public class p_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my first project at sliit ");
+
+	}
+
+}
